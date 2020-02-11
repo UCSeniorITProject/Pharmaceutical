5b5c3007658ee37af6fa2a074de474bda62571c3
Samuel Curry <samc735@gmail.com>
2020-02-11 14:07:39 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  db: {
+    host: process.env.dbHost || 'safemeds.database.windows.net',
+    dialect: 'mssql',
+    maxConnectionSockets: 10,
+    minConnectionSockets: 5,
+    connectionAcquisitionRate: 30000,
+    databaseName: process.env.dbName || 'SafeMeds',
+    username: process.env.adminUser || 'qweqweqwe',
+    password: process.env.password || 'qweqweqwe',
+    connectionIdleRate: 10000,
+    port: 1433,
+    //DO NOT TURN THIS TO TRUE YOU WILL DROP EVERY TABLE
+    forceTableCreation: false,
+    shouldLog: true,
+	},
+	encryptionKey: process.env.encryptionKey || 'qweqweqweqwe',
+  jwtDurationMinutes: '15',
+  jwtRefreshDurationHours: '20',
+  saltRounds: 8,
+  jwtSecret: process.env.jwtSecret || 'qweqweqweqwe',
+  jwtRefreshTokenSecret: process.env.jwtRefreshTokenSecret || 'qweqweqqweqweqweweqwe',
+  shouldFastifyLog: true,
+  serverHost:  process.env.serverAddress || '0.0.0.0' ,
+  port: process.env.port || '3004',
+};
